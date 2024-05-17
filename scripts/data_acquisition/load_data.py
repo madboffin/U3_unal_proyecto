2d820dfd964ec12544e58d86d57ce14bab903696
manuel huertas <manuelhuertasfl@gmail.com>
2024-05-16 19:41:30 -0500

    agregar depdendencias a archivo load_data

--- scripts/data_acquisition/load_data.py
+++ scripts/data_acquisition/load_data.py
@@ -1,14 +1,18 @@
+import os
+
+import pandas as pd
+import gdown
+
 def load_data(download_data: bool = False) -> pd.DataFrame:
     file_id = "1D31Z6sUCVUynRyimxs2n8rrULnlwhP9l"
     url = f"https://drive.google.com/uc?id={file_id}"
-    output = "src/txt_comments/jigsaw_data.zip"
+    output_file = "src/txt_comments/jigsaw_data.zip"
     if download_data:
         if not os.path.exists("src/jigsaw"):
             os.makedirs("src/txt_comments")
-        gdown.download(url, output)
+        gdown.download(url, output_file)
 
-    df = pd.read_csv(output, dtype=str)
+    df = pd.read_csv(output_file, dtype=str)
     print(f"El dataset tiene {df.shape[0]} filas y {df.shape[1]} columnas.")
-
 
     return df
