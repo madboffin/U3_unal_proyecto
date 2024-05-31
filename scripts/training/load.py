f936ad181eaa2274ce4b457ae5add9c043bd67ce
manuel huertas <manuelhuertasfl@gmail.com>
2024-05-30 23:37:21 -0500

    agregar funcion get_trian_test_split

--- scripts/training/load.py
+++ scripts/training/load.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV
+import numpy as np
 
 
 def load_model_data():
@@ -8,3 +10,11 @@ def load_model_data():
     """
     file_sampled = r"src\txt_comments\jigsaw_data_prep_sampled.parquet"
     return pd.read_parquet(file_sampled)
+
+
+def get_train_test_split(test_size: float = 0.2, seed: int = 42) -> tuple:
+    """Get the train test split"""
+    df = load_model_data()
+    features = df["comment_vec"].to_list()
+    target = df["class"].to_list()
+    return train_test_split(features, target, test_size=test_size, random_state=seed)
